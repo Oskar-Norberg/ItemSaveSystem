90fc193ade8da56e449cf4377dbfab163b7a0c55
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-04-17 23:56:44 +0200

    ADD: [Script] SerializableGuid

---
+++ Assets/_Scripts/SerializableGuid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _Project.SaveSystem
+{
+    [Serializable]
+    public struct SerializableGuid
+    {
+        public string GuidString;
+        
+        public SerializableGuid(Guid guid)
+        {
+            GuidString = guid.ToString();
+        }
+        
+        public SerializableGuid(string guidString)
+        {
+            GuidString = guidString;
+        }
+
+        public static implicit operator bool(SerializableGuid serializableGuid)
+        {
+            return !string.IsNullOrEmpty(serializableGuid.GuidString);
+        }
+    }
+}
