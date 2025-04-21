747a696e8a19dd44c3cb3e3d13123d0370c90ba4
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-04-21 14:38:21 +0200

    ADD: [Script] ISerializer

---
+++ Assets/_Scripts/Interfaces/ISerializer.cs
@@ -0,0 +1,20 @@
+namespace _Project.SaveSystem.Interfaces
+{
+    public interface ISerializer
+    {
+        /**
+         * <summary>Serialize and save SaveData to path.</summary>
+         * <param name="saveData">The SaveData to serialize.</param>
+         * <param name="path">The path to save the data to. File extension is chosen in serializer.</param>
+         * <returns>True upon success. False on failure.</returns>
+         */
+        bool Serialize(SaveData saveData, string path);
+        
+        /**
+         * <summary>Deserialize and load LoadedData from path.</summary>
+         * <param name="path">The path to load the data from. File extensions handled by serializer.</param>
+         * <returns>The loaded data.</returns>
+         */
+        LoadedData Deserialize(string path);
+    }
+}
