9b2d05f60bfb780e1c8a27054b6aa5b2b89742b1
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-04-21 21:06:07 +0200

    EDIT: [Script] Implement SaveFileService

--- Assets/_Scripts/SaveFileService.cs
+++ Assets/_Scripts/SaveFileService.cs
@@ -8,5 +8,33 @@ namespace _Project.SaveSystem
 {
     public class SaveFileService
     {
+        private const string SaveFileName = "creatively_named_save_file";
+        
+        private ISerializer _serializer;
+        
+        public SaveFileService()
+        {
+            _serializer = ServiceLocator.Instance.GetService<ISerializer>();
+        }
+
+        // TODO: Implement save slots
+        // TODO: Don't completely overwrite the file. Append to it instead.
+        public void SaveToFile(uint slot, HeadSaveData saveData)
+        {
+            string serializedOutput = _serializer.Serialize(saveData);
+            File.WriteAllText(GetPathString(), serializedOutput);
+        }
+
+        public HeadSaveData LoadFromFile(uint slot)
+        {
+            string serializedData = File.ReadAllText(GetPathString());
+
+            return _serializer.Deserialize<HeadSaveData>(serializedData);
+        }
+        
+        private static string GetPathString()
+        {
+            return Path.Combine(Application.persistentDataPath, SaveFileName);
+        }
     }
 }
