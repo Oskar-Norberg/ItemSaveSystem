8b7d21e9bb9b2960c33a97881db27ea6b981adbc
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-04-21 15:01:01 +0200

    EDIT: [Script] Set up JSON-Serializer

--- Assets/_Scripts/DataLoading/JSON/JSONSerializer.cs
+++ Assets/_Scripts/DataLoading/JSON/JSONSerializer.cs
@@ -1,17 +1,67 @@
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace _Project.SaveSystem.Interfaces.DataLoading.JSON
 {
     public class JSONSerializer : ISerializer
     {
+        private const string FileExtension = ".json";
+        
         public bool Serialize(List<Saveable> saveables, string path)
         {
-            throw new System.NotImplementedException();
+            HeadJSONContainer headJSONContainer = new HeadJSONContainer();
+            
+            foreach (var saveable in saveables)
+            {
+                var saveDatas = saveable.GetSaveData();
+            
+                SubJSONContainer container = new SubJSONContainer
+                {
+                    GUID = saveable.GUID,
+                    SaveableType = saveable.SaveableType,
+                    Data = saveDatas
+                };
+                
+                headJSONContainer.AddSubContainer(container);
+            }
+            
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            StreamWriter streamWriter = new StreamWriter(path + FileExtension);
+            JsonWriter writer = new JsonTextWriter(streamWriter);
+            
+            jsonSerializer.TypeNameHandling = TypeNameHandling.Objects;
+            writer.Formatting = Formatting.Indented;
+            
+            jsonSerializer.Serialize(writer, headJSONContainer);
+            
+            writer.Close();
+            streamWriter.Close();
+
+            return true;
         }
 
         public ILoadedData Deserialize(string path)
         {
-            throw new System.NotImplementedException();
+            if (!File.Exists(path))
+            {
+                // TODO: Change this to a TryDeserialize method that returns a bool. (This should return false)
+                return null;
+            }
+            
+            // TODO: Move this to a separate Serializer/Deserializer class.
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            StreamReader streamReader = new StreamReader(path + FileExtension);
+            JsonReader reader = new JsonTextReader(streamReader);
+            
+            jsonSerializer.TypeNameHandling = TypeNameHandling.Objects;
+                
+            HeadJSONContainer headJSONContainer = jsonSerializer.Deserialize<HeadJSONContainer>(reader);
+            
+            reader.Close();
+            streamReader.Close();
+            
+            return new JSONLoadedData(headJSONContainer);
         }
     }
 }
