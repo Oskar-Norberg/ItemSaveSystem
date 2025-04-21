5a3e86fe82057f7751b4d28cf84c7cf59866549e
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-04-21 21:05:59 +0200

    ADD: [Script] SaveFileService

---
+++ Assets/_Scripts/SaveFileService.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using _Project.SaveSystem.DataLoading.Common;
+using _Project.SaveSystem.Interfaces;
+using ringo.ServiceLocator;
+using UnityEngine;
+
+namespace _Project.SaveSystem
+{
+    public class SaveFileService
+    {
+    }
+}
