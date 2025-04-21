f16847becf9f3e22448d23aa70855a71eaf469be
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-04-21 20:34:26 +0200

    EDIT: [Script] Remove Serializer saving to file

--- Assets/_Scripts/DataLoading/Interfaces/ISerializer.cs
+++ Assets/_Scripts/DataLoading/Interfaces/ISerializer.cs
@@ -2,21 +2,19 @@ namespace _Project.SaveSystem.Interfaces
 {
     public interface ISerializer
     {
-        // TODO: This is now inexplicably linked to the Saveable class. This should be made more generic. This is just a serializer. Not explicitly a part of the save system.
-        // TODO: this should return a string rather than saving it to a file.
         /**
-         * <summary>Serialize and save object to path.</summary>
+         * <summary>Serialize object and return as string.</summary>
          * <param name="objectToSerialize">The object to serialize.</param>
-         * <param name="path">The path to save the data to. File extension is chosen in serializer.</param>
-         * <returns>True upon success. False on failure.</returns>
+         * <returns>String representing serialized object.</returns>
          */
-        bool Serialize(object objectToSerialize, string path);
+        string Serialize(object objectToSerialize);
         
         /**
-         * <summary>Deserialize and load LoadedData from path.</summary>
-         * <param name="path">The path to load the data from. File extensions handled by serializer.</param>
+         * <summary>Deserialize object.</summary>
+         * <typeparam name="T">Type to deserialize object to.</typeparam>
+         * <param name="serializedObject">String representing object to deserialize.</param>
          * <returns>The loaded data.</returns>
          */
-        T Deserialize<T>(string path);
+        T Deserialize<T>(string serializedObject);
     }
 }
