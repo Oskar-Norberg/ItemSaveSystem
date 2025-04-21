1673a94f319a73d63c739200005f271bfdc065dc
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-04-21 20:03:55 +0200

    EDIT: [Script] Make ISerializer more generic

--- Assets/_Scripts/DataLoading/Interfaces/ISerializer.cs
+++ Assets/_Scripts/DataLoading/Interfaces/ISerializer.cs
@@ -1,24 +1,22 @@
-using System.Collections.Generic;
-using _Project.SaveSystem.Interfaces.DataLoading;
-
 namespace _Project.SaveSystem.Interfaces
 {
     public interface ISerializer
     {
         // TODO: This is now inexplicably linked to the Saveable class. This should be made more generic. This is just a serializer. Not explicitly a part of the save system.
+        // TODO: this should return a string rather than saving it to a file.
         /**
-         * <summary>Serialize and save SaveData to path.</summary>
-         * <param name="saveData">The SaveData to serialize.</param>
+         * <summary>Serialize and save object to path.</summary>
+         * <param name="objectToSerialize">The object to serialize.</param>
          * <param name="path">The path to save the data to. File extension is chosen in serializer.</param>
          * <returns>True upon success. False on failure.</returns>
          */
-        bool Serialize(List<Saveable> saveables, string path);
+        bool Serialize(object objectToSerialize, string path);
         
         /**
          * <summary>Deserialize and load LoadedData from path.</summary>
          * <param name="path">The path to load the data from. File extensions handled by serializer.</param>
          * <returns>The loaded data.</returns>
          */
-        ILoadedData Deserialize(string path);
+        T Deserialize<T>(string path);
     }
 }
