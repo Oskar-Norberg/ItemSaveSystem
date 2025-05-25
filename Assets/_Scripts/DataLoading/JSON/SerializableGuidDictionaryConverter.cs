74895f7ff43ea2eb8a7b618f228f77cba8bc8d51
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-05-25 19:31:36 +0200

    ADD: [Script] SerializableGuidDictionaryConverter

---
+++ Assets/_Scripts/DataLoading/JSON/SerializableGuidDictionaryConverter.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using _Project.SaveSystem.DataLoading.Common;
+
+namespace _Project.SaveSystem.Interfaces.DataLoading.JSON
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+
+    public class SerializableGuidDictionaryConverter<T> : JsonConverter<Dictionary<SerializableGuid, T>> 
+    {
+        public override void WriteJson(JsonWriter writer, Dictionary<SerializableGuid, T> value, JsonSerializer serializer)
+        {
+            writer.WriteStartArray();
+            
+            foreach (var kvp in value)
+            {
+                writer.WriteStartObject();
+                
+                writer.WritePropertyName("Key");
+                serializer.Serialize(writer, kvp.Key);
+                
+                writer.WritePropertyName("Value");
+                serializer.Serialize(writer, kvp.Value);
+                
+                writer.WriteEndObject();
+            }
+            
+            writer.WriteEndArray();
+        }
+
+        public override Dictionary<SerializableGuid, T> ReadJson(JsonReader reader, Type objectType, Dictionary<SerializableGuid, T> existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            var list = serializer.Deserialize<List<KeyValuePair<SerializableGuid, T>>>(reader);
+            return list.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        }
+    }
+}
