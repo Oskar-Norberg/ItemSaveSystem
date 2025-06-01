e01c192f9a4549e2e5fa69df48207845ed60d66f
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-06-01 23:35:49 +0200

    ADD: [Script] MonoSaveSubsystem

---
+++ Assets/_Scripts/SaveSubsystem/MonoSaveSubsystem.cs
@@ -0,0 +1,22 @@
+using _Project.SaveSystem.SaveLoader;
+using UnityEngine;
+
+namespace _Project.SaveSystem.Subsystem
+{
+    public abstract class MonoSaveSubsystem : MonoBehaviour, ISaveSubsystem
+    {
+        public abstract object GetSaveData();
+
+        public abstract void Load(object saveData);
+
+        void ISaveSubsystem.Register(ISaveLoader saveManager)
+        {
+            saveManager.RegisterSaveSubsystem(this);
+        }
+
+        void ISaveSubsystem.Unregister(ISaveLoader saveManager)
+        {
+            saveManager.UnregisterSaveSubsystem(this);
+        }
+    }
+}
