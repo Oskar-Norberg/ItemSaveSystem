1713e354489fa66904ebee951e6dead43221276f
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-06-24 13:27:14 +0200

    ADD: [Script] SaveLoadStages

---
+++ Assets/_Scripts/SaveSystem/SaveSubsystem/SaveLoadStages.cs
@@ -0,0 +1,16 @@
+namespace ringo.SaveSystem.Subsystem
+{
+    public enum SaveStage
+    {
+        PreSave,
+        Save,
+        PostSave,
+    }
+    
+    public enum LoadStage
+    {
+        PreLoad,
+        Load,
+        PostLoad,
+    }
+}
