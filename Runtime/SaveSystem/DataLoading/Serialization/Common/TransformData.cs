7d33e0a9a1c1575216581aaf59f7464c2a8ef708
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-06-29 12:01:19 +0200

    EDIT: [Script] TransformData pos, rot, scale constructor

--- Runtime/SaveSystem/DataLoading/Serialization/Common/TransformData.cs
+++ Runtime/SaveSystem/DataLoading/Serialization/Common/TransformData.cs
@@ -7,5 +7,12 @@ namespace ringo.SaveSystem.DataLoading.Serialization.Common
         public Vector3 Position;
         public Quaternion Rotation;
         public Vector3 Scale;
+        
+        public TransformData(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            Position = position;
+            Rotation = rotation;
+            Scale = scale;
+        }
     }
 }
