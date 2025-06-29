064abf1a2ea92ac01f1fa086bdc86e24772c5f78
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-06-29 11:54:20 +0200

    ADD: [Script] TransformData/TransformDataConverter

---
+++ Runtime/SaveSystem/DataLoading/Serialization/Common/TransformData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace ringo.SaveSystem.DataLoading.Serialization.Common
+{
+    public struct TransformData
+    {
+        public Vector3 Position;
+        public Quaternion Rotation;
+        public Vector3 Scale;
+    }
+}
